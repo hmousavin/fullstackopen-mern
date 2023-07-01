08b50d5aeabfa1aa2f4eede12ae5a5eabce3fa04
mrv90 <hamid.mousavinasab@gmail.com>
2023-07-01 19:35:40 +0330

    Delete part3/phonebook directory

--- part3/phonebook/frontend/src/components/FilterComponent.js
+++
@@ -1,7 +0,0 @@
-const FilterComponent = ({setFilter}) => {
-    return (
-        <div>filter shown with <input onChange={(e) => setFilter(e.target.value)}></input></div>
-    )
-}
-
-export default FilterComponent;
