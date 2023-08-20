b7af7ed745f2959dde14bd6a16bd97e402b2d94a
mrv90 <hamid.mousavinasab@gmail.com>
2023-08-20 21:56:27 +0330

    Add files via upload

--- part5/bloglist/backend/controllers/testing.js
+++ part5/bloglist/backend/controllers/testing.js
@@ -1,10 +1,18 @@
 const testingRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const bcrypt = require('bcrypt')
 
 testingRouter.post('/reset', async (request, response) => {
   await Blog.deleteMany({})
   await User.deleteMany({})
+
+  const passwordHash = await bcrypt.hash('salainen', 10)
+  await new User({
+    name: 'mluukkai salainen',
+    username: 'mluukkai',
+    passwordHash: passwordHash,
+  }).save()
 
   response.status(204).end()
 })
