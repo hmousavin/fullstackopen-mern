bffe41fd3d703cba6fa067cb15bae089c468a0ca
mrv90 <hamid.mousavinasab@gmail.com>
2023-11-07 21:51:13 +0330

    Delete part7/new-bloglist directory

--- part7/new-bloglist/client/src/components/BlogForm.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export function BlogForm(props) {
-  return <>BlogForm</>;
-}
--- part7/new-bloglist/client/src/components/BlogList.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export function BlogList(props) {
-  return <>BlogList</>;
-}
