d3053ab00a04414a6c6c6dd6b78bdce77de96ef5
mrv90 <hamid.mousavinasab@gmail.com>
2024-04-24 11:39:33 +0330

    working on 8.17 problem

--- part8/ReactAndGraphQL/library-frontend/src/App.js
+++ part8/ReactAndGraphQL/library-frontend/src/App.js
@@ -2,12 +2,39 @@ import { useState } from 'react'
 import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
+import Notification from './components/Notification'
+import LoginForm from './components/LoginForm'
+import { useApolloClient } from '@apollo/client'
 
 const App = () => {
   const [page, setPage] = useState('authors')
+  const [token, setToken] = useState(null)
+  const [notification, setNotification] = useState({message: '', type: 'unknown'})
+  const client = useApolloClient()
+
+  const logout = () => {
+    setToken(null)
+    localStorage.clear()
+    client.resetStore()
+  }
+
+  if (!token) {
+    return (
+      <div>
+        <Notification message={notification.message} type={'failure'} />
+        <h2>Login</h2>
+        <LoginForm
+          setToken={setToken}
+          setError={setNotification}
+        />
+      </div>
+    )
+  }
 
   return (
     <div>
+      <Notification message={notification.message} type={'failure'} />
+      <button onClick={logout}>logout</button>
       <div>
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
--- part8/ReactAndGraphQL/library-frontend/src/components/Books.js
+++ part8/ReactAndGraphQL/library-frontend/src/components/Books.js
@@ -1,4 +1,6 @@
 import { gql, useQuery } from '@apollo/client'
+import { useState, useEffect } from 'react'
+import Notification from './Notification'
 
 const ALL_BOOKS = gql`
   query {
@@ -12,26 +14,26 @@ const ALL_BOOKS = gql`
     }
   } 
 `
-
 const Books = (props) => {
   const result = useQuery(ALL_BOOKS)
+  const [notification, setNotification] = useState({message: '', type: 'unknown'})
 
+  useEffect(() => {
+    if (result.error) 
+      setNotification({message: result.error.message, type: 'failure'})
+  }, [result.error])
+  
   if (!props.show)
     return null
-  
-  if (result.loading) {
-    return <div>loading...</div>
-  }
-
-  if (result.error) {
-    console.error(result.error)
-    return <div>Error!</div>
-  }
 
   return (
     <div>
       <h2>books</h2>
-
+      <Notification 
+        key={new Date().valueOf()} // this id makes a new component, each time !
+        message={notification.message} type={notification.type}/>
+      {result.loading ? <div>loading...</div> : 
+      result.error ? <div></div> :
       <table>
         <tbody>
           <tr>
@@ -47,7 +49,7 @@ const Books = (props) => {
             </tr>
           ))}
         </tbody>
-      </table>
+      </table>}
     </div>
   )
 }
