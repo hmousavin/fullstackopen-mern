ffd401fd0ab9fbcf62460634d2a9cec3853c349c
mrv90 <hamid.mousavinasab@gmail.com>
2024-05-06 11:31:57 +0330

    solutions of exercises: 9.14 & 9.15

---
+++ part9/react-with-typescript/src/components/Total.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { CoursePart } from "../types";
+
+const Total = ({ parts }: { parts: CoursePart[] }) => {
+    const total = parts.reduce((carry, part) => carry + part.exerciseCount, 0);
+    return <p>Number of exercises {total}</p>;
+};
+
+export default Total;
